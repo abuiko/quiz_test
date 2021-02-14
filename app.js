428aec66f3cfa471d652254ad091a539ebe39622
Alena Buiko <abuiko.ma@gmail.com>
2021-02-13 23:46:58 -0500

    create quizData array

---
+++ app.js
@@ -0,0 +1,43 @@
+const quizData = [{
+        question: 'Question 1',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd'
+    },
+    {
+        question: 'Question 2',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd'
+    },
+    {
+        question: 'Question 3',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd'
+    },
+    {
+        question: 'Question 4',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd'
+    },
+    {
+        question: 'Question 5',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd'
+    },
+    {
+        question: 'Question 6',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd'
+    }
+]
