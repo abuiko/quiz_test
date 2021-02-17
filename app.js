8280b98e0793e1672aee68208032af6903829852
Alena Buiko <abuiko.ma@gmail.com>
2021-02-17 12:23:27 -0500

    load quiz

--- app.js
+++ app.js
@@ -1,43 +1,65 @@
 const quizData = [{
-        question: 'Question 1',
-        a: 'a',
-        b: 'b',
-        c: 'c',
-        d: 'd'
+        question: 'Who is the president of USA?',
+        a: 'a1',
+        b: 'b1',
+        c: 'c1',
+        d: 'd1'
     },
     {
-        question: 'Question 2',
-        a: 'a',
-        b: 'b',
-        c: 'c',
-        d: 'd'
+        question: 'How many oceans are there on our planet?',
+        a: 'a2',
+        b: 'b2',
+        c: 'c2',
+        d: 'd2'
     },
     {
-        question: 'Question 3',
-        a: 'a',
-        b: 'b',
-        c: 'c',
-        d: 'd'
+        question: 'Who invented electricity?',
+        a: 'a3',
+        b: 'b3',
+        c: 'c3',
+        d: 'd3'
     },
     {
-        question: 'Question 4',
-        a: 'a',
-        b: 'b',
-        c: 'c',
-        d: 'd'
+        question: 'When did WW2 started?',
+        a: 'a4',
+        b: 'b4',
+        c: 'c4',
+        d: 'd4'
     },
     {
-        question: 'Question 5',
-        a: 'a',
-        b: 'b',
-        c: 'c',
-        d: 'd'
+        question: 'What is the capital of US?',
+        a: 'a5',
+        b: 'b5',
+        c: 'c5',
+        d: 'd5'
     },
     {
-        question: 'Question 6',
-        a: 'a',
-        b: 'b',
-        c: 'c',
-        d: 'd'
+        question: 'What is the capital of Belarus?',
+        a: 'a6',
+        b: 'b6',
+        c: 'c6',
+        d: 'd6'
     }
 ]
+
+
+
+const questionEl = document.querySelector('.quiz-question');
+const a_text = document.getElementById('a-text');
+const b_text = document.getElementById('b-text');
+const c_text = document.getElementById('c-text');
+const d_text = document.getElementById('d-text');
+
+let currentQuiz = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    const currentQuizData = quizData[currentQuiz];
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+    currentQuiz++;
+}
